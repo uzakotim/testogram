1f982f47b0c59c201b7f211a4ec5406ade4529f1
uzakotim <timauzakov@gmail.com>
2023-02-03 17:01:51 +0100

    over network success

---
+++ python/network_test/getch.py
@@ -0,0 +1,13 @@
+def getch():
+    import sys, termios, tty
+
+    fd = sys.stdin.fileno()
+    orig = termios.tcgetattr(fd)
+
+    try:
+        tty.setcbreak(fd)  # or tty.setraw(fd) if you prefer raw mode's behavior.
+        return sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSAFLUSH, orig)
+
+#source https://stackoverflow.com/questions/3523174/raw-input-without-pressing-enter
