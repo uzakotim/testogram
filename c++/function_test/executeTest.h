e17f07a8d5f7f441f6bca1b5adf918c7f4a6baf6
uzakotim <timauzakov@gmail.com>
2023-02-15 12:42:50 +0100

    removing sources from website

--- c++/function_test/executeTest.h
+++
@@ -1,9 +0,0 @@
-// Function to write commands to produce tests
-/* ----------------------------------------------------- */
-#include "handleTest.h"
-void execute_tests()
-{
-    // handleTest(testNumber, input, reference)
-    handleTest(1,1, "1");    
-}
-/* ----------------------------------------------------- */
